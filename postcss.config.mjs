15a60e08bb4db51696e5f15b6ef1e2de3315234b
fj3lg4d0 <franciscojesusdelgadovazquez@outlook.com>
2025-04-03 00:27:26 +0200

    Merged remote main into local main

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
   },
+=======
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+>>>>>>> 8ebe165 (Saving local changes before rebase)
 };
 
 export default config;
